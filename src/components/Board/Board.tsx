3b8b595dd49dca1be3e6e2b017deb0d4f13d9f1a
GaliaBonda <galina.rizhniak@gmail.com>
2022-07-20 21:57:06 +0300

    bug fix for board: display none on index

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -9,26 +9,10 @@ type Props = {
 
 export default function Board(props: Props) {
     const { tickets } = props;
-    const todo: JSX.Element[] = [];
-    const inprogress: JSX.Element[] = [];
-    const done: JSX.Element[] = [];
-
     const [shownTicketsNum, setshownTicketsNum] = useState(10);
-
-    if (tickets) tickets.forEach((item, index) => {
-        let ticket = <li className="board__list-item" key={item.id}
-            style={index <= shownTicketsNum ? { display: 'block' } : { display: 'none' }}>
-            <Ticket title={item.title} id={item.id} status={item.status} userId={item.userId}
-                userName={item.userName} color={item.color} boardStatus={true} /></li>;
-
-        if (item.status === 'to do') {
-            todo.push(ticket);
-        } else if (item.status === 'in progress') {
-            inprogress.push(ticket);
-        } else if (item.status === 'done') {
-            done.push(ticket);
-        }
-    });
+    const todo = tickets?.filter((item) => item.status === 'to do');
+    const inprogress = tickets?.filter((item) => item.status === 'in progress');
+    const done = tickets?.filter((item) => item.status === 'done');
 
     return (
         <div className="board">
@@ -36,15 +20,27 @@ export default function Board(props: Props) {
             <div className="board__inner">
                 <ul className="board__list todo-list">
                     <h3 className="board__subtitle">To do</h3>
-                    {todo}
+                    {todo.map((item, index) => {
+                        return (index <= shownTicketsNum && <li className="board__list-item" key={item.id}>
+                            <Ticket title={item.title} id={item.id} status={item.status} userId={item.userId}
+                                userName={item.userName} color={item.color} boardStatus={true} /></li>);
+                    })}
                 </ul>
                 <ul className="board__list inprogress-list">
                     <h3 className="board__subtitle">In progress</h3>
-                    {inprogress}
+                    {inprogress.map((item, index) => {
+                        return (index <= shownTicketsNum && <li className="board__list-item" key={item.id}>
+                            <Ticket title={item.title} id={item.id} status={item.status} userId={item.userId}
+                                userName={item.userName} color={item.color} boardStatus={true} /></li>);
+                    })}
                 </ul>
                 <ul className="board__list done-list">
                     <h3 className="board__subtitle">Done</h3>
-                    {done}
+                    {done.map((item, index) => {
+                        return (index <= shownTicketsNum && <li className="board__list-item" key={item.id}>
+                            <Ticket title={item.title} id={item.id} status={item.status} userId={item.userId}
+                                userName={item.userName} color={item.color} boardStatus={true} /></li>);
+                    })}
                 </ul>
                 {(shownTicketsNum < props.tickets.length) && <button className="app-btn board__btn"
                     onClick={() => setshownTicketsNum(props.tickets.length)} >Show all...</button>}
